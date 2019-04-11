018a96cc7e2cb4f5c4c7db1222fa4bbda4e48d33
narpskii <hyoowin@gmail.com>
2019-04-11 16:26:58 -0700

    stats panel text display
    displays total number of vibes earned

---
+++ Assets/Scripts/StatsFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatsFunctions : MonoBehaviour
+{
+    [SerializeField] public PlayerManager data;
+    public TextMeshProUGUI clickTotalText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = FindObjectOfType<PlayerManager>();
+        clickTotalText.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clickTotalText.text = "Total little kids: " + data.player.clickTotal;
+    }
+}
