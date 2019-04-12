ce6ac451dce954d763f7c8abcbdc3b0c21755e73
narpskii <hyoowin@gmail.com>
2019-04-11 20:53:05 -0700

    critters+
    new critters and critter animations
    ticket placeholder

--- Assets/Scripts/MainMenuFunctions.cs
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class MainMenuFunctions : MonoBehaviour
 {
+
+    private int randomizer;
+
     public Image critter;
     public Image critterTwo;
+    public Image critterThree;
+    public Image critterFour;
 
     [SerializeField] bool looping = false;
+    bool initial = true;
 
+    Animation a, aTwo;
 
-    Animation a;
     /*
     public void Start()
     {
@@ -29,13 +35,95 @@ public class MainMenuFunctions : MonoBehaviour
 
             a = critterTwo.GetComponent<Animation>();
             a.Play("lozanoPeek");
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(6f);
+
+            a = critter.GetComponent<Animation>();
+            a.Play("critterJiggleBack");
+            yield return new WaitForSeconds(1f);
+
+            StartCoroutine(critterAnim());
+
         } while (looping);
         
         
     }
+    
+    public IEnumerator critterAnim()
+    {
+        if(initial) //run this section only once
+        {
+            aTwo = critterThree.GetComponent<Animation>();
+            aTwo.Play("JossEnter");
+            yield return new WaitForSeconds(1f);
 
+            aTwo = critterFour.GetComponent<Animation>();
+            aTwo.Play("lozanoEnter");
+            yield return new WaitForSeconds(4f);
 
+            //aTwo = critterFour.GetComponent<Animation>();
+            aTwo.Play("lozanoLeave");
+            yield return new WaitForSeconds(0.1f);
+
+            aTwo = critterThree.GetComponent<Animation>();
+            aTwo.Play("jossLeave");
+            yield return new WaitForSeconds(5f);
+        }
+
+        initial = false;
+
+        randomizer = Random.Range(1, 5);
+        switch (randomizer)
+        {
+            case 1:
+                aTwo = critterThree.GetComponent<Animation>();
+                aTwo.Play("jossSlide");
+                yield return new WaitForSeconds(0.5f);
+
+                aTwo = critterFour.GetComponent<Animation>();
+                aTwo.Play("lozanoSlide");
+                yield return new WaitForSeconds(4f);
+                break;
+            case 2:
+                aTwo = critterThree.GetComponent<Animation>();
+                aTwo.Play("jossSlide1");
+                yield return new WaitForSeconds(0.5f);
+
+                aTwo = critterFour.GetComponent<Animation>();
+                aTwo.Play("lozanoSlide1");
+                yield return new WaitForSeconds(4f);
+
+                break;
+            case 3:
+                aTwo = critterThree.GetComponent<Animation>();
+                aTwo.Play("jossSlide2");
+                yield return new WaitForSeconds(0.5f);
+
+                aTwo = critterFour.GetComponent<Animation>();
+                aTwo.Play("lozanoSlide2");
+                yield return new WaitForSeconds(4f);
+                break;
+
+            case 4:
+                aTwo = critterThree.GetComponent<Animation>();
+                aTwo.Play("jossSlide");
+                yield return new WaitForSeconds(0.5f);
+
+                aTwo = critterFour.GetComponent<Animation>();
+                aTwo.Play("lozanoSlide");
+                yield return new WaitForSeconds(2f);
+
+                aTwo = critterThree.GetComponent<Animation>();
+                aTwo.Play("jossSlide1");
+                yield return new WaitForSeconds(0.5f);
+
+                aTwo = critterFour.GetComponent<Animation>();
+                aTwo.Play("lozanoSlide1");
+                yield return new WaitForSeconds(4f);
+                break;
+        }
+
+    }
+    
 
        
 
