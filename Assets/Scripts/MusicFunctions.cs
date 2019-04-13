b2f8d3761c78921d1e2483d2a64c4ad514a7477b
narpskii <hyoowin@gmail.com>
2019-04-13 16:00:30 -0700

    Update MusicFunctions.cs

--- Assets/Scripts/MusicFunctions.cs
+++ Assets/Scripts/MusicFunctions.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class MusicFunctions : MonoBehaviour
 {
 
     private MusicPlayer currentSong;
 
-    public TextMeshProUGUI songNameDisplay;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         currentSong = FindObjectOfType<MusicPlayer>();
+        
     }
+
+   
 
     public void NextSong()
     {
